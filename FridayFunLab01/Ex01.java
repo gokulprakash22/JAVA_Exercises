3c7f3d640e940ff958cbcc8cc4a2358a4bf2ae3a
Gokul Prakash P <gokulprakash22@gmail.com>
2021-03-01 13:33:37 +0530

    FridayFunLab01 programs added

---
+++ FridayFunLab01/Ex01.java
@@ -0,0 +1,14 @@
+package FridayFunLab01;
+
+public class Ex01 {
+	 public static void main(String[] args) {
+	 int a, b;
+	 a = 10;
+	 b = 20;
+	 System.out.println("Before swapping : a, b = "+a+", "+ + b);
+	 a = a + b;
+	 b = a - b;
+	 a = a - b;
+	 System.out.println("After swapping : a, b = "+a+", "+ + b);
+	 }
+	}
