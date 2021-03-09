a5869e111d647e5571e3501d06a2ef2431ea209d
Gokul Prakash P <gokulprakash22@gmail.com>
2021-03-09 14:05:22 +0530

    Add files via upload

---
+++ mycodes/day12/ExecutorThreadFactoryDemo.java
@@ -0,0 +1,21 @@
+package multithreading;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors; 
+import java.util.concurrent.ThreadFactory;
+
+public class ExecutorThreadFactoryDemo {
+	static Thread t;
+	public static void main(String[] args) {
+		ExecutorService es=Executors.newFixedThreadPool(1,new ThreadFactory() {
+			 @Override
+			 public Thread newThread(Runnable r) {
+			 t=Executors.defaultThreadFactory().newThread(r);
+			 t.setName("thread factory thread...");
+			 System.out.println("thread...................");
+			 return t;
+			 }
+		 });
+		 es.execute(()->{System.out.println("thread called...........");});
+		 System.out.println(t);
+	 }
+}
