7381e9a8dbfeb8d0f7db05098d3a1a55ba3142eb
Gokul Prakash P <gokulprakash22@gmail.com>
2021-03-09 13:57:43 +0530

    Add files via upload

---
+++ mycodes/day09/fridayFunLab01/Ex03.java
@@ -0,0 +1,13 @@
+package fridayFunLab01;
+import java.util.Scanner;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		String s;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a string");
+		s = scan.nextLine();
+		StringBuffer buffer=new StringBuffer(s);
+		System.out.println(buffer.reverse());
+	}
+}
