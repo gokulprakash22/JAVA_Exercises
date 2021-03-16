572339f1a29e4885ef40f8d3f97e12f1b0bede17
Gokul Prakash P <gokulprakash22@gmail.com>
2021-03-16 11:35:37 +0530

    Add files via upload

---
+++ mycodes/day17/Dictionary_ta.java
@@ -0,0 +1,14 @@
+package internationalization;
+
+import java.util.ListResourceBundle;
+
+public class Dictionary_ta extends ListResourceBundle{
+	Object obj[][]= {
+			{"hello","vanakkam"}
+	};
+	@Override
+	protected Object[][] getContents() {
+		// TODO Auto-generated method stub
+		return obj;
+	}
+}
