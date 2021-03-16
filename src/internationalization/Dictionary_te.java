c82981cf3464c3ac2f919201dc06b3da0e05c170
Gokul Prakash P <gokulprakash22@gmail.com>
2021-03-16 11:34:48 +0530

    Add files via upload

---
+++ src/internationalization/Dictionary_te.java
@@ -0,0 +1,13 @@
+package internationalization;
+
+import java.util.ListResourceBundle;
+public class Dictionary_te extends ListResourceBundle{
+	Object obj[][]= {
+			{"hello","namaskaramulu"}
+	};
+	@Override
+	protected Object[][] getContents() {
+		// TODO Auto-generated method stub
+		return obj;
+	}
+}
