d8a287047a76aa6ce212783c07d88ab085e2f460
Gokul Prakash P <gokulprakash22@gmail.com>
2021-03-31 01:01:24 +0530

    Delete Components directory

--- Components/src/invoice_rmi_server/Invoice.java
+++
@@ -1,7 +0,0 @@
-package invoice_rmi_server;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Invoice extends Remote{
-	public void startInvoiceApp(int choice) throws RemoteException;
-}
